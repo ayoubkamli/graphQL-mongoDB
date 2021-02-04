cc1036362f87db177fa0359801d5496a23e2f2a9
Ayyoub Kamli <akamli@e1r5p8.1337.ma>
2021-02-04 12:57:32 +0100

    get the first request

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import ApolloClient, { gql } from "apollo-boost";
 
-function App() {
-  return <p>hello</p>;
-}
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+});
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  client
+    .query({
+      query: gql`
+        {
+          allPosts {
+            id
+            title
+            description
+          }
+        }
+      `,
+    })
+    .then((result) => setPosts(result.data.allPosts));
+
+  return <p>{JSON.stringify(posts)}</p>;
+};
 
 export default App;
