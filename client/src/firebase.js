74d51ea2e661c57a980dea8d9e7de2e087fdf39b
Ayyoub Kamli <akamli@e1r4p8.1337.ma>
2021-02-14 19:05:15 +0100

    Firebase context reducer setup client side

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from firebase
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCGfi1HLcwUf80yJrcgWeAqJI6NYDX3H6k",
+  authDomain: "reactnode-1.firebaseapp.com",
+  projectId: "reactnode-1",
+  storageBucket: "reactnode-1.appspot.com",
+  // messagingSenderId: "596961187481",
+  appId: "1:596961187481:web:8252d387c21c8c81ebca08",
+  measurementId: "G-CJNWQ4JVL8",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+export const auth = firebase.auth()
+export const googleAuthProvider = new firebase.auth.googleAuthProvider()
